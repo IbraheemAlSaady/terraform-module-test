bb4658b186f2dcc307863724133d713bba526213
ibraheem Al Saady <ibraheemalsaady@ibraheems-MacBook-Pro-2.local>
2022-05-29 21:35:47 +0200

    added a random_integer resource without put

--- main.tf
+++ main.tf
@@ -9,6 +9,15 @@ resource "random_string" "random" {
   override_special = "/@£$"
 }
 
+resource "random_integer" "number" {
+  min = 1
+  max = 40
+}
+
 output "result" {
   value = random_string.random.result
+}
+
+output "number" {
+  value = random_integer.number.result
 }
